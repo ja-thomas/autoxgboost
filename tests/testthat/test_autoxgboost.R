d38a8585499fe00ef96a57a5a0fb913a3a061ba0
Janek Thomas <janek.thomas@web.de>
2017-05-12 15:26:43 +0200

    more tests

--- tests/testthat/test_autoxgboost.R
+++ tests/testthat/test_autoxgboost.R
@@ -2,12 +2,13 @@ context("autoxgboost")
 
 test_that("autoxgboost works on different tasks",  {
 
-  checkAutoxgboost = function(task, build.final.model, control, mbo.learner) {
-    r = autoxgboost(task, build.final.model = build.final.model, max.nrounds = 1L, control = control, mbo.learner = mbo.learner, nthread = 1)
+  checkAutoxgboost = function(task, build.final.model, factor.encoder, control, mbo.learner) {
+    r = autoxgboost(task, build.final.model = build.final.model, max.nrounds = 1L,
+      factor.encoder = factor.encoder, control = control, mbo.learner = mbo.learner, nthread = 1)
     td = getTaskDesc(task)
 
     expect_class(r, "AutoxgbResult")
-    if (sum(td$n.feat[c("factors", "ordered")]) > 0) {
+    if (sum(td$n.feat[c("factors", "ordered")]) > 0 & factor.encoder == "impact") {
       expect_class(r$final.learner, "ImpactFeatureWrapper")
     } else {
       expect_class(r$final.learner, "RLearner")
@@ -17,7 +18,11 @@ test_that("autoxgboost works on different tasks",  {
 
     extras = names(r$optim.result$opt.path$env$extra[[11]])
     expect_subset("nrounds", extras) # check that nrounds is in extras
-  expect_equal(16, nrow(as.data.frame(r$optim.result$opt.path))) # check that opt.path has right number of rows
+    expect_equal(16, nrow(as.data.frame(r$optim.result$opt.path))) # check that opt.path has right number of rows
+
+    p = predict(r, newdata = getTaskData(task))
+    expect_class(p, "Prediction")
+
   }
 
   iris.fac = droplevels(iris[1:100,])
@@ -31,8 +36,10 @@ test_that("autoxgboost works on different tasks",  {
     subsetTask(bh.task, subset = 1:50)
   )
 
-  for (t in tasks) {
-      checkAutoxgboost(task = t, build.final.model = TRUE, control = ctrl, mbo.learner = mbo.learner) #check default
+  for (im in c("impact", "dummy")) {
+    for (t in tasks) {
+      checkAutoxgboost(task = t, build.final.model = TRUE, factor.encoder = im, control = ctrl, mbo.learner = mbo.learner) #check default
+    }
   }
 
 })
